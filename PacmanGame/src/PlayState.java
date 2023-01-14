e72a2d066cb6b86cc98fef5c0b1cdbb207caea87
Smiles <chenmay1696@gmail.com>
2023-01-14 20:51:51 +0200

    feat: addition of pacman and ghosts

--- PacmanGame/src/PlayState.java
+++ PacmanGame/src/PlayState.java
@@ -1,68 +1,78 @@
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.util.ArrayList;
+import java.util.List;
+import java.awt.Graphics;
 
 public class PlayState extends GameState {
-
 	private boolean active;
 	private boolean gameOver;
 	private int score;
 	private float deltaTimeAverage;
 	private String message;
 	private Pacman pacman;
-	private ArrayList<GameObject> ghosts;
-	private ArrayList<Coin> coins;
-	
+	private List<Ghost> ghosts;
+	private List<Coin> coins;
+
 	public PlayState() {
 		score = 0;
 		gameOver = false;
 		pacman = new Pacman();
 		ghosts = new ArrayList<>();
 		coins = new ArrayList<>();
-		ghosts.add(new GameObject(550, 100, 50, 50));
+		ghosts.add(new Ghost(300, 200));
+		ghosts.add(new Ghost(160, 200));
+		ghosts.add(new Ghost(230, 100));
+		ghosts.add(new Ghost(340, 320));
 		coins.add(new Coin(300, 120));
 		coins.add(new Coin(350, 120));
 		coins.add(new Coin(400, 120));
 	}
-	
+
 	public void enter(Object memento) {
 		active = true;
 		deltaTimeAverage = 0;
 		if (gameOver) {
 			pacman.reset();
 			resetCoins();
+			resetGhosts();
 			score = 0;
 			gameOver = false;
 		}
 	}
-	
+
 	public void processKeyReleased(int aKeyCode) {
+		pacman.processKeyReleased(aKeyCode);
 		if (aKeyCode == KeyEvent.VK_ESCAPE)
 			System.exit(0);
-		
+
 		if (aKeyCode == KeyEvent.VK_Q)
 			active = false;
-			
+
 	}
-	
+
 	public void update(long deltaTime) {
 		if (gameOver) {
-			if(!pacman.shrink())
-				active = false;
-		}
-		else {
-			deltaTimeAverage = deltaTimeAverage* 0.9f + 0.1f*(float)deltaTime;
-			float x = pacman.getX() + 0.1f*deltaTime;
-			if (x > Game.WIDTH)
-				x = x - Game.WIDTH;
-			pacman.setX(x);
+			active = false;
+		} else {
+			deltaTimeAverage = deltaTimeAverage * 0.9f + 0.1f * (float) deltaTime;
+			pacman.pacmanMovement(deltaTime);
+			ghostUpdate(deltaTime);
 			ghostsCollision();
 			coinsCollision();
 		}
 	}
-	
-	public boolean isActive() { return active; }
-	
+
+	public void ghostUpdate(long deltaTime) {
+		for (Ghost boo : ghosts) {
+			boo.ghostMovement(deltaTime);
+		}
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
 	public String next() {
 		return "Welcome";
 	}
@@ -76,8 +86,10 @@ public class PlayState extends GameState {
 	}
 
 	private void ghostsCollision() {
-		if (pacman.checkCollisionArray(ghosts))
-			gameOver = true;
+		for (Ghost boo : ghosts) {
+			if (boo.checkCollisionArray(pacman))
+				gameOver = true;
+		}
 	}
 
 	private void coinsCollision() {
@@ -92,15 +104,23 @@ public class PlayState extends GameState {
 			c.setVisible(true);
 	}
 
+	private void resetGhosts() {
+		for (Ghost boo : ghosts) {
+			boo.reset();
+		}
+
+	}
+
 	private void drawPacman(Graphics g) {
-		g.setColor(Color.yellow);
-		g.fillOval((int)pacman.getX(), (int)pacman.getY(), pacman.getWidth(), pacman.getHeight());
+		g.drawImage(pacman.getImage(), (int) pacman.getX(), (int) pacman.getY(), null);
 	}
 
 	private void drawGhosts(Graphics g) {
-		g.setColor(Color.red);
-		for (GameObject o : ghosts)
-			g.fillOval((int)o.getX(), (int)o.getY(), o.getWidth(), o.getHeight());
+
+		for (Ghost boo : ghosts) {
+			g.drawImage(boo.getImage(), (int) boo.getX(), (int) boo.getY(), null);
+		}
+
 	}
 
 	private void drawCoins(Graphics g) {
